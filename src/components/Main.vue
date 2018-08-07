f06a5257134db859f848b0bed9776148fba38d6a
maxoon <desfouxmaxime@gmail.com>
2018-08-07 12:01:46 +0200

    add  :axios and request to api films

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex wrap flexCenter mb-xxl mt-xxl" id="main-cont">
     <app-film v-for="(film,i) in movieState.films" :key="i" v-bind="{film}"></app-film>
-  <!--  <app-modal class="hide" :class="{show: movieState.choosenFilm}"></app-modal>-->
-      <app-modal v-if="movieState.choosenFilm"></app-modal>
+    <!--  <app-modal class="hide" :class="{show: movieState.choosenFilm}"></app-modal>-->
+    <app-modal v-if="movieState.choosenFilm"></app-modal>
   </div>
 </template>
 
@@ -10,9 +10,12 @@
 <script>
   import Film from './Film.vue';
   import AppModal from './Modal.vue';
+  import axios from 'axios';
   import {
     movieState
   } from '../states/movieState';
+
+  const FILMS_API = 'http://localhost:8005/films';
   
   export default {
     name: 'AppMain',
@@ -28,22 +31,22 @@
     },
     methods: {
       async filmsServed() {
+  
         try {
-          const result = await fetch('/data/movies.json');
-          const films = await result.json();
-          this.movieState.films = films['films'];
+          const result = await axios.get(FILMS_API);
+          this.movieState.films = result.data['films'];
           this.movieState.films = this.movieState.films.concat(this.movieState.films).concat(this.movieState.films).concat(this.movieState.films)
   
         } catch (e) {
           console.log(e);
         }
       },
-
+  
   
     },
     async created() {
       this.filmsServed();
-
+  
     },
   
   
